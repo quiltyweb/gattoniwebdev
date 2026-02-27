abb96aa415c1578c011edaf731e9e101d1325c37
dgattoni <danigattoni@gmail.com>
2026-02-27 17:12:33 +1100

    chore: add basic logic to install turbo rails to update contact form response

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,10 +3,13 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.valid?
-      # temporarily just print to console, but here we will integrate with the email service to send the message to us
-      print "Contact is valid. Name: #{@contact.name}, Email: #{@contact.email}, Message: #{@contact.message}"
+      respond_to do |format|
+        format.turbo_stream { render :create }
+      end
     else
-       print "Contact is invalid. Errors: #{@contact.errors.full_messages.join(", ")}"
+      respond_to do |format|
+        format.turbo_stream { render :create, status: :unprocessable_entity }
+      end
     end
   end
  
