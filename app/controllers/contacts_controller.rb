121240d7823f5bb0575ba74b632f8ea39497094b
dgattoni <danigattoni@gmail.com>
2026-03-03 22:55:05 +1100

    chore: try form attempt 2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,14 +9,22 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.valid?
-      ContactMailer.with(
-        sender_name: contact_params[:name],
-        sender_email: contact_params[:email],
-        message_body: contact_params[:message]
-      ).contact_message.deliver_now
-      
-      respond_to do |format|
-        format.turbo_stream { render :create }
+      begin
+        ContactMailer.with(
+          sender_name: contact_params[:name],
+          sender_email: contact_params[:email],
+          message_body: contact_params[:message]
+        ).contact_message.deliver_now
+        
+        respond_to do |format|
+          format.turbo_stream { render :create }
+        end
+      rescue StandardError => e
+        Rails.logger.error "[CONTACT FORM ERROR]"
+        Rails.logger.error "Class: #{e.class}"
+        Rails.logger.error "Message: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+        handle_error("Mail service error")
       end
     else
       respond_to do |format|
@@ -31,4 +39,12 @@ class ContactsController < ApplicationController
     params.require(:contact).permit(:name, :email, :message)
   end
 
+  def handle_error(message)
+    flash.now[:alert] = message
+
+    respond_to do |format|
+      format.turbo_stream { render :create, status: :unprocessable_entity }
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
 end
